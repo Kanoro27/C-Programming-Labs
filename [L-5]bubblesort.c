75cfa06ccea8a2c99856ad35b5abe1fc2ba1146b
Praveen Kumar S <78217016+Kanoro27@users.noreply.github.com>
2022-05-31 16:21:20 +0530

    Sorting an Array

---
+++ [L-5]bubblesort.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main() {
+    int n,a[20],temp,x,y;
+    printf("Enter the Number of Elements : ");
+    scanf("%d",&n);
+    for (x=0;x<n;x++) {
+        printf("Enter the Element %d : ",x+1);
+        scanf("%d",&a[x]);
+    }
+    for (x=0;x<n-1;x++) {
+        for (y=0;y<n-1-x;y++) {
+            if (a[y]>a[y+1]) {
+                temp=a[y];
+                a[y]=a[y+1];
+                a[y+1]=temp;
+            } 
+        }
+    }
+    printf("The Sorted Array is : ");
+    for (x=0;x<n;x++) {
+        printf("%d\t",a[x]);
+    }   
+}
